1329171242411aebbce04256a74a5ecf242b02c9
landercor <asprillafabian034@gmail.com>
2026-03-08 23:43:31 -0500

    Moví archivos y modifiqué Usuario1.java

--- .history/Pruebas-git/Usuario1_20260203145140.java
+++ .history/Pruebas-git/Usuario1_20260203145140.java
@@ -3,16 +3,14 @@ public class Usuario1 {
 
     String nombre;
     int edad;
-    boolean esActivo;
 
     public Usuario1(String nombre, int edad) {
         this.nombre = nombre;
         this.edad = edad;
-        this.esActivo = true;
     }
 
     public void mostrarInfo() {
-        System.out.println("Nombre: " + nombre + " | Edad: " + edad + " | Activo: " + esActivo);
+        System.out.println("Nombre: " + nombre + " | Edad: " + edad);
     }
 
 }
