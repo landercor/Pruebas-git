733297b0c42219df6b59e94b05ef90830c213117
landercor <asprillafabian034@gmail.com>
2026-03-12 18:40:35 -0500

    Delete file

--- do-while.java
+++
@@ -1,8 +0,0 @@
-
-public static void main(String[] args) {
-    for (int i = 1; i <= 10 ; i++) {
-        if (i % 2 == 0) {
-            System.out.println("es par");
-        }
-    }
-}
--- for3.java
+++
@@ -1,6 +0,0 @@
-
-public static void main(String[] args) {
-    for (int i = 10; i >= 1; i--) {
-            System.out.println(i);
-        }
-    }
