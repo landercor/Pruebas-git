5076caec512fa85886c78d63c0938e975e977eb6
landercor <asprillafabian034@gmail.com>
2026-03-11 23:38:23 -0500

    agregando archivos prueba

--- previous/i10.java
+++
@@ -1,9 +0,0 @@
-
-public class i10 {
-
-    public static void main(String[] args) {
-        for (int i = 0, j = 4; i <= j; i++, j--) {
-            System.out.println("i: " + i + ", j: " + j);
-        }
-    }
-}
